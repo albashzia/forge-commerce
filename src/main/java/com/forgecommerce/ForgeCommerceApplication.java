cbd2053b6eab97ebea8125ec22eac3c311e32fe5
albashzia <ahmedalbashzia@gmail.com>
2026-09-13 15:45:07 +0500

    chore: initialize Spring Boot project

---
+++ src/main/java/com/forgecommerce/ForgeCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.forgecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgeCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/forge_commerce/ForgeCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package forge_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
