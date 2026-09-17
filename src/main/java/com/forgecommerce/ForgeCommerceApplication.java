f45a55cdc61aaa1d702364bc2c9d85c2fee50fff
albashzia <ahmedalbashzia@gmail.com>
2026-09-17 14:55:37 +0500

    refactor: remove redundant access specifier from main method in ForgeCommerceApplication class

--- src/main/java/com/forgecommerce/ForgeCommerceApplication.java
+++ src/main/java/com/forgecommerce/ForgeCommerceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ForgeCommerceApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ForgeCommerceApplication.class, args);
 	}
 
