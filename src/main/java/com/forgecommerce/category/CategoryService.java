2834962e445be5cde48c0fb7a5db80a0bf342fae
albashzia <ahmedalbashzia@gmail.com>
2026-09-18 19:40:44 +0500

    feat : add CategoryService as a service layer between controller and repository

---
+++ src/main/java/com/forgecommerce/category/CategoryService.java
@@ -0,0 +1,11 @@
+package com.forgecommerce.category;
+
+import java.util.List;
+
+public interface CategoryService {
+    Category createCategory(Category category);
+    Category getCategoryById(Long id);
+    List<Category> getAllCategories();
+    Category updateCategory(Long id, Category category);
+    void deleteCategory(Long id);
+}
