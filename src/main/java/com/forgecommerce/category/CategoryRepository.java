b3dda9c245345ef9c896fe61f20cf352f9a34adc
albashzia <ahmedalbashzia@gmail.com>
2026-09-19 20:01:44 +0500

    docs: add explanatory comments to CategoryRepository interface

--- src/main/java/com/forgecommerce/category/CategoryRepository.java
+++ src/main/java/com/forgecommerce/category/CategoryRepository.java
@@ -1,5 +1,8 @@
 package com.forgecommerce.category;
+
+// Provides built-in CRUD and database access methods
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Repository interface responsible for database operations related to database
 public interface CategoryRepository extends JpaRepository<Category,Long>{
 }
