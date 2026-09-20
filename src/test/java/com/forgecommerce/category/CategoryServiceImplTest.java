aa9957622a27fa6328e9326a73f4c39a240267ef
albashzia <ahmedalbashzia@gmail.com>
2026-09-20 21:12:18 +0500

    docs: add explanatory comments to CategoryServiceImplTest class

--- src/test/java/com/forgecommerce/category/CategoryServiceImplTest.java
+++ src/test/java/com/forgecommerce/category/CategoryServiceImplTest.java
@@ -10,25 +10,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+
+@ExtendWith(MockitoExtension.class) // Tells JUnit to use MockitoExtension for this class
 public class CategoryServiceImplTest {
 
-    @Mock
+    @Mock // Creates a Mock of CategoryRepository
     private CategoryRepository categoryRepository;
 
+    // Creates the actual CategoryServiceImpl object to test.
     @InjectMocks
     private CategoryServiceImpl categoryService;
 
-    @Test
+    @Test // @Test tells JUnit that this method is a test case
     void createCategory(){
+
+        // Arrange
         Category category = new Category();
         category.setName("Electronics");
         category.setDescription("Electrical products");
         when(categoryRepository.save(category)).thenReturn(category);
 
+        // Act
         Category result = categoryService.createCategory(category);
 
+        // Assert
         assertEquals(category,result);
+
+        // Verify
         verify(categoryRepository).save(category);
     }
 }
